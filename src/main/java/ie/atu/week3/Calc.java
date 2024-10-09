ec07631e295c35b9623d1cf9d090e36e6827e1a2
James Donnellan <G00434671@atu.ie>
2024-10-09 20:19:15 +0100

    Added a new method for subtraction

--- src/main/java/ie/atu/week3/Calc.java
+++ src/main/java/ie/atu/week3/Calc.java
@@ -15,15 +15,15 @@ public class Calc
         String op = sc.next();
         switch(op)
         {
-            case "add": add(a, b); break;
+            case "subtract": subtract(a,b); break;
             default: System.out.println("Invalid operation"); break;
 
         }
 
     }
-    public static void add(int a, int b)
+    public static void subtract(int a, int b)
     {
-        int sum = a + b;
+        int sum = a - b;
         System.out.println("The sum is: " + sum);
     }
 }
